e0b47c96dee38f1e984b33e01d968026e7ee283d
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-10-02 17:30:55 +0300

    feature: добавить кастомные исключения

---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/InvalidUser.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.filmorate.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class InvalidUser extends RuntimeException{
+
+
+    public InvalidUser(String message){
+        super(message);
+    }
+}
