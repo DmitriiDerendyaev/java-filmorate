ba93eabee7d87f794b0258a3103bedf59b81c817
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-10-02 23:13:26 +0300

    style: добавить пробелы и отступы в InvalidDurationException

--- src/main/java/ru/yandex/practicum/filmorate/exception/InvalidDurationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/InvalidDurationException.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class InvalidDurationException extends RuntimeException{
-    public InvalidDurationException(String message){
+public class InvalidDurationException extends RuntimeException {
+    public InvalidDurationException(String message) {
         super(message);
     }
 }
