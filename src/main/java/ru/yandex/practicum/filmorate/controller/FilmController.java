c3d6e663ac1c445c2a5845e407fb65ec7960d66d
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-10-02 17:31:25 +0300

    feature: добавить логику работы с объектами, доавить логирование

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,28 +1,41 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.InvalidDurationException;
+import ru.yandex.practicum.filmorate.exception.InvalidFilm;
 import ru.yandex.practicum.filmorate.exception.InvalidReleaseDateException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import javax.validation.Valid;
+import java.time.Duration;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/films")
 public class FilmController {
     private HashMap<Integer, Film> films = new HashMap<>();
+    private int currentId = 1;
 
     @GetMapping()
     public List<Film> getAllFilms(){
+        log.info("Fetching all films");
+
         return new ArrayList<>(films.values());
     }
 
     @PostMapping
     public Film addFilm(@RequestBody @Valid Film film) {
-        validateReleaseDate(film.getReleaseDate());
+        validateReleaseDate(film);
+        validateDuration(film);
+
+        film.setId(currentId++);
+        log.info("Adding a new film: {}", film);
+
         films.put(film.getId(), film);
 
         return film;
@@ -30,16 +43,35 @@ public class FilmController {
 
     @PutMapping
     public Film updateFilm(@RequestBody @Valid Film film){
-        validateReleaseDate(film.getReleaseDate());
+        if(!films.containsKey(film.getId())){
+            log.warn("Unknown film: {}", film);
+            throw new InvalidFilm("Unknown film");
+        }
+        validateReleaseDate(film);
+        validateDuration(film);
+
+        log.info("Update film {} to film: {} ", film, films.get(film.getId()));
+
         films.put(film.getId(), film);
 
         return film;
     }
 
-    private void validateReleaseDate(LocalDate releaseDate) {
+    private void validateReleaseDate(Film film) {
+        LocalDate releaseDate = film.getReleaseDate();
         LocalDate minReleaseDate = LocalDate.of(1895, 12, 28);
         if (releaseDate.isBefore(minReleaseDate)) {
+            log.warn("Release date must not be earlier than 28 December 1895, but is: {}", releaseDate);
             throw new InvalidReleaseDateException("Release date must not be earlier than 28 December 1895");
+        }
+    }
+
+    private void validateDuration(Film film){
+        Duration duration = film.getDurationValue();
+
+        if(duration.isNegative()){
+            log.warn("Duration cannot be negative.");
+            throw new InvalidDurationException("Duration cannot be negative.");
         }
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,6 +1,8 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.InvalidUser;
 import ru.yandex.practicum.filmorate.model.User;
 
 import javax.validation.Valid;
@@ -9,26 +11,38 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     private HashMap<Integer, User> users = new HashMap<>();
+    private int currentId = 1;
 
     @GetMapping()
     public List<User> getAllUsers(){
+        log.info("Fetching all users");
+
         return new ArrayList<>(users.values());
     }
 
     @PostMapping()
-    public User addUser(@RequestBody @Valid User user){
+    public User addUser(@Valid @RequestBody User user){
+        user.setId(currentId++);
+        log.info("Adding a new user: {}", user);
+
         users.put(user.getId(), user);
 
         return user;
     }
 
     @PutMapping
-    public User updateUser(@RequestBody @Valid User user){
+    public User updateUser(@Valid @RequestBody User user){
+        if (!users.containsKey(user.getId())){
+            log.warn("Unknown user: {}", user);
+            throw new InvalidUser("Unknown user");
+        }
+        log.info("Update user {} to user: {} ", user, users.get(user.getId()));
         users.put(user.getId(), user);
 
         return user;
