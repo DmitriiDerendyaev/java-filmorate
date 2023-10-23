5d37e04d3393d699cb1939ed25f87348d6d6b9b3
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-10-23 23:54:59 +0300

    feature: добавить классы для отлова ошибок

--- src/main/java/ru/yandex/practicum/filmorate/handler/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/handler/ErrorHandler.java
@@ -1,15 +1,71 @@
 package ru.yandex.practicum.filmorate.handler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmorate.exception.DataAlreadyExistException;
+import ru.yandex.practicum.filmorate.exception.DataNotFoundException;
+import ru.yandex.practicum.filmorate.exception.InvalidDurationException;
+import ru.yandex.practicum.filmorate.exception.InvalidReleaseDateException;
 
+import javax.validation.ValidationException;
+
+@RestControllerAdvice
 public class ErrorHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleMethodArgumentTypeMismatch(MethodArgumentNotValidException ex) {
-        String message = "Invalid parameter type: " + ex.getMessage();
-        return ResponseEntity.badRequest().body(message);
+    private static final Logger log = LoggerFactory.getLogger(ErrorHandler.class);
+
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    public ResponseEntity<String> handleMethodArgumentTypeMismatch(MethodArgumentNotValidException ex) {
+//        String message = "Invalid parameter type: " + ex.getMessage();
+//        return ResponseEntity.badRequest().body(message);
+//    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationException(final ValidationException e) {
+        log.info("400 {}", e.getMessage());
+        return new ErrorResponse("Request error", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleAlreadyExistException(final DataAlreadyExistException e) {
+        log.info("409 {}", e.getMessage());
+        return new ErrorResponse("Conflict error: ", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleThrowable(final Throwable e) {
+        log.info("500 {}", e.getMessage(), e);
+        return new ErrorResponse("Server error: ", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFoundException(final DataNotFoundException e) {
+        log.info("404 {}", e.getMessage());
+        return new ErrorResponse("Element not found: ", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse invalidReleaseDateException(final InvalidReleaseDateException e) {
+        log.info("400 {}", e.getMessage());
+        return new ErrorResponse("Release error: ", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse invalidDurationException(final InvalidDurationException e) {
+        log.info("400 {}", e.getMessage());
+        return new ErrorResponse("Duration error: ", e.getMessage());
     }
 
 }
