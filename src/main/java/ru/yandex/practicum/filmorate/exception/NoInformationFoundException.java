861f67a136747cc8f6666041c824a179529cec1d
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-11-22 00:31:49 +0300

    feature: Добавить ключевые exceptions, TODO: изменить handler

---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NoInformationFoundException.java
@@ -0,0 +1,6 @@
+package ru.yandex.practicum.filmorate.exception;
+public class NoInformationFoundException extends RuntimeException {
+    public NoInformationFoundException(String message) {
+        super(message);
+    }
+}
