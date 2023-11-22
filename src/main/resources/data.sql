b0441b22c34c77e0eb8efb293f9919951277f777
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-11-23 00:50:00 +0300

    refactor: изменить логику схемы БД, логики ее удаления и предварительной подготовки, добавлен в коммит data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO mpa (mpa_id, rating) values (1, 'G');
+INSERT INTO mpa (mpa_id, rating) values (2, 'PG');
+INSERT INTO mpa (mpa_id, rating) values (3, 'PG-13');
+INSERT INTO mpa (mpa_id, rating) values (4, 'R');
+INSERT INTO mpa (mpa_id, rating) values (5, 'NC-17');
+
+INSERT INTO genres (genre_id, name) values (1, 'Комедия');
+INSERT INTO genres (genre_id, name) values (2, 'Драма');
+INSERT INTO genres (genre_id, name) values (3, 'Мультфильм');
+INSERT INTO genres (genre_id, name) values (4, 'Триллер');
+INSERT INTO genres (genre_id, name) values (5, 'Документальный');
+INSERT INTO genres (genre_id, name) values (6, 'Боевик');
