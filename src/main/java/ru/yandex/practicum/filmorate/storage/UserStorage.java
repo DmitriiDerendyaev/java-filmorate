0b893aeb3e78a806a75dfb068b97ea41f300c412
Dmitrii Derendyaev <dds_stalker@mail.ru>
2023-11-23 21:13:20 +0300

    delete: удалить @Deprecated классы реализации хранения в памяти компьютера

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++
@@ -1,9 +0,0 @@
-package ru.yandex.practicum.filmorate.storage;
-
-import ru.yandex.practicum.filmorate.model.User;
-
-@Deprecated
-public interface UserStorage<T extends User> extends StorageCRUD<User> {
-
-    public User getById(Long userId);
-}
